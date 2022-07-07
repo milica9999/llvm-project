16d81b13464405c29515d2a22d946a9870f64776
Milica Kovacevic <milica.kovacevic@syrmia.com>
2022-07-07 14:48:42 +0200

    Added one more CTTZ test

--- llvm/test/Transforms/cttz_test_with_opt.c
+++ llvm/test/Transforms/cttz_test_with_opt.c
@@ -1,15 +1,10 @@
-// RUN: clang -target riscv64-linux-unknown-elf -no-canonical-prefixes --gcc-toolchain=/home/syrmia/gcc/riscv-toolchain/install --sysroot=/home/syrmia/gcc/riscv-toolchain/install/sysroot -O2 -S %s -emit-llvm -static -o - | opt -aggressive-instcombine -mtriple riscv64-linux-elf -S -o - | /home/syrmia/Desktop/llvm/llvm-project/build/bin/FileCheck %s
+// RUN: clang -target riscv64-linux-unknown-elf -no-canonical-prefixes --gcc-toolchain=/home/syrmia/gcc/riscv-toolchain/install --sysroot=/home/syrmia/gcc/riscv-toolchain/install/sysroot -O2 -S %s -Xclang -no-opaque-pointers -emit-llvm -static -o - | opt -aggressive-instcombine -mtriple riscv64-linux-elf -S -o - | /home/syrmia/Desktop/llvm/llvm-project/build/bin/FileCheck %s
 
 static const char u = 0;
 // CHECK: entry:
-// CHECK: %sub = sub i32 0, %x
-// CHECK: %and = and i32 %sub, %x
-// CHECK: %mul = mul i32 %and, 72416175
-// CHECK: %shr = lshr i32 %mul, 26
-// CHECK: %idxprom = zext i32 %shr to i64
-// CHECK: %arrayidx = getelementptr inbounds [64 x i8], ptr @ntz_seals_alg.table, i64 0, i64 %idxprom
-// CHECK: %0 = load i8, ptr %arrayidx, align 1, !tbaa !6
-// CHECK: %conv = zext i8 %0 to i32
+// CHECK: %0 = call i32 @llvm.cttz.i32(i32 %x, i1 false)
+// CHECK: %1 = trunc i32 %0 to i8
+// CHECK: %conv = zext i8 %1 to i32
 // CHECK: ret i32 %conv
 int ntz_seals_alg(unsigned x) {
   static char table[64] = {32, 0,  1,  12, 2,  6,  u,  13, 3,  u,  7,  u, u,
@@ -25,15 +20,11 @@ int ntz_seals_alg(unsigned x) {
 // CHECK: %cmp = icmp eq i32 %x, 0
 // CHECK: br i1 %cmp, label %return, label %if.end
 
-// CHECK: if.end:                                        
-// CHECK: %sub = sub i32 0, %x
-// CHECK: %and = and i32 %sub, %x
-// CHECK: %mul = mul i32 %and, 81224991
-// CHECK: %shr = lshr i32 %mul, 27
-// CHECK: %idxprom = zext i32 %shr to i64
-// CHECK: %arrayidx = getelementptr inbounds [32 x i8], ptr @ntz_deBruijn_cycle.table, i64 0, i64 %idxprom
-// CHECK: %0 = load i8, ptr %arrayidx, align 1, !tbaa !6
-// CHECK: %conv = zext i8 %0 to i32
+// CHECK: if.end:                                         
+// CHECK: %0 = call i32 @llvm.cttz.i32(i32 %x, i1 true)
+// CHECK: %1 = icmp eq i32 %x, 0
+// CHECK: %2 = trunc i32 %0 to i8
+// CHECK: %conv = zext i8 %2 to i32
 // CHECK: br label %return
 
 // CHECK: return:                                           
@@ -55,11 +46,11 @@ int ntz_deBruijn_cycle(unsigned x) {
 // CHECK: %and = and i32 %sub, %x
 // CHECK: %rem = urem i32 %and, 37
 // CHECK: %idxprom = zext i32 %rem to i64
-// CHECK: %arrayidx = getelementptr inbounds [37 x i8], ptr @ntz_reisers_slg.table, i64 0, i64 %idxprom
-// CHECK: %0 = load i8, ptr %arrayidx, align 1, !tbaa !6
+// CHECK: %arrayidx = getelementptr inbounds [37 x i8], [37 x i8]* @ntz_reisers_alg.table, i64 0, i64 %idxprom
+// CHECK: %0 = load i8, i8* %arrayidx, align 1, !tbaa !6
 // CHECK: %conv = zext i8 %0 to i32
 // CHECK: ret i32 %conv
-int ntz_reisers_slg(unsigned x) {
+int ntz_reisers_alg(unsigned x) {
   static char table[37] = {32, 0, 1,  26, 2,  23, 27, u,  3,  16, 24, 30, 28,
                            11, u, 13, 4,  7,  17, u,  25, 22, 31, 15, 29, 10,
                            12, 6, u,  21, 14, 9,  5,  20, 8,  19, 18};
